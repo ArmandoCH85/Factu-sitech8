5c9dab2ad48adfa4f1f6f7da6b04eaac0f001652
Cristian Vega <cristianvega73212828@gmail.com>
2026-01-05 18:08:51 -0500

    feat(pdf): Plantillas oficiales: default y marca de agua | Creación de comando fallback para usuarios con otros templates en sus establecimientos

--- database/seeders/TenancyDatabaseSeeder.php
+++ database/seeders/TenancyDatabaseSeeder.php
@@ -72,114 +72,114 @@ class TenancyDatabaseSeeder extends Seeder
 
         //! Rellenar los warehouse_id desde inventories hasta items
 
-        $items_warehouse_null = DB::table('items')->where("warehouse_id", '=', null)->get();
+        // $items_warehouse_null = DB::table('items')->where("warehouse_id", '=', null)->get();
 
-        $items_warehouse_null->each(function($item, $key){
-            $inventory_warehouse_id = DB::table('inventories')->where('item_id', $item->id)->first();
-            if ($inventory_warehouse_id) {
-                DB::table('items')->where('id', '=',$item->id )->update([
-                    "warehouse_id" => $inventory_warehouse_id->warehouse_id
-                ]);
-            }
-        });
+        // $items_warehouse_null->each(function($item, $key){
+        //     $inventory_warehouse_id = DB::table('inventories')->where('item_id', $item->id)->first();
+        //     if ($inventory_warehouse_id) {
+        //         DB::table('items')->where('id', '=',$item->id )->update([
+        //             "warehouse_id" => $inventory_warehouse_id->warehouse_id
+        //         ]);
+        //     }
+        // });
 
 
         /**
          * ! Mandar los datos de DispatchAddress a PersonAddress
          */
 
-        $dispatch_address_data = DB::table("dispatch_addresses")->get();
+        // $dispatch_address_data = DB::table("dispatch_addresses")->get();
 
-        if ($dispatch_address_data->count() > 0) {
+        // if ($dispatch_address_data->count() > 0) {
 
-            // Siempre va a validar cuando ya un tenant exista y además si tiene valores dentro de la tabla dispatch_addresses
-            $configurations = DB::table("configurations")->first();
-            if ($configurations->is_migrated_address) {
-                return;
-            }
+        //     // Siempre va a validar cuando ya un tenant exista y además si tiene valores dentro de la tabla dispatch_addresses
+        //     $configurations = DB::table("configurations")->first();
+        //     if ($configurations->is_migrated_address) {
+        //         return;
+        //     }
 
 
-            $data_insert_all = collect([]);
+        //     $data_insert_all = collect([]);
 
-            // Se crea un nuevo array pero en la llave location_id, en vez de tener los valors en json, se tendra un array con todo los valores parseados
-            $dispatch_address_data->transform(function ($item, $key)  {
+        //     // Se crea un nuevo array pero en la llave location_id, en vez de tener los valors en json, se tendra un array con todo los valores parseados
+        //     $dispatch_address_data->transform(function ($item, $key)  {
 
-                //Convierto el objeto estandar en un array asociativo
-                $objec_dispatch_addresses = collect( (array) $item);
+        //         //Convierto el objeto estandar en un array asociativo
+        //         $objec_dispatch_addresses = collect( (array) $item);
 
-                $parse_item = $objec_dispatch_addresses->map(function($item_object, $key_object){
-                    if ($key_object == "location_id") {
-                        // |  country_id |  department_id(0)  | province_id)(1)  | district_id(2)  |
-                        $location_clear = json_decode($item_object);
+        //         $parse_item = $objec_dispatch_addresses->map(function($item_object, $key_object){
+        //             if ($key_object == "location_id") {
+        //                 // |  country_id |  department_id(0)  | province_id)(1)  | district_id(2)  |
+        //                 $location_clear = json_decode($item_object);
 
-                        $ubigeo_deparment = DB::table('departments')->get();
-                        $ubigeo_province = DB::table('provinces')->get();
-                        $ubigeo_district = DB::table('districts')->get();
+        //                 $ubigeo_deparment = DB::table('departments')->get();
+        //                 $ubigeo_province = DB::table('provinces')->get();
+        //                 $ubigeo_district = DB::table('districts')->get();
 
-                        // Encontrar si tiene un id valido en la los datos ubigeo
+        //                 // Encontrar si tiene un id valido en la los datos ubigeo
 
-                        $deparment_find = $ubigeo_deparment->where("id", $location_clear[0]);
-                        $province_find = $ubigeo_province->where("id", $location_clear[1]);
-                        $district_find = $ubigeo_district->where("id", $location_clear[2]);
+        //                 $deparment_find = $ubigeo_deparment->where("id", $location_clear[0]);
+        //                 $province_find = $ubigeo_province->where("id", $location_clear[1]);
+        //                 $district_find = $ubigeo_district->where("id", $location_clear[2]);
 
-                        // Si no es un caracter valido dentro de las base de datos, entonces se devolverá un null
-                        $deparment = $deparment_find->count() == 0 ? null : $location_clear[0];
-                        $province = $province_find->count() == 0 ? null : $location_clear[1];
-                        $district = $district_find->count() == 0 ? null : $location_clear[2];
+        //                 // Si no es un caracter valido dentro de las base de datos, entonces se devolverá un null
+        //                 $deparment = $deparment_find->count() == 0 ? null : $location_clear[0];
+        //                 $province = $province_find->count() == 0 ? null : $location_clear[1];
+        //                 $district = $district_find->count() == 0 ? null : $location_clear[2];
 
-                        return ['PE', $deparment ,$province, $district ];
-                    } else {
-                        return $item_object;
-                    }
-                });
+        //                 return ['PE', $deparment ,$province, $district ];
+        //             } else {
+        //                 return $item_object;
+        //             }
+        //         });
 
-                return $parse_item->all();
+        //         return $parse_item->all();
 
-            });
+        //     });
 
-            // Tabla de person address
-            // | person_id | country_id (PE) | department_id | province_id | district_id | address | phone | email | main | establishment_code
+        //     // Tabla de person address
+        //     // | person_id | country_id (PE) | department_id | province_id | district_id | address | phone | email | main | establishment_code
 
-            $array_dispatch_data = $dispatch_address_data->all();
-           foreach ($array_dispatch_data as $data_dispatch) {
-             $array_data_insert_one = [];
+        //     $array_dispatch_data = $dispatch_address_data->all();
+        //    foreach ($array_dispatch_data as $data_dispatch) {
+        //      $array_data_insert_one = [];
 
-             //person_id
-             $array_data_insert_one["person_id"] = $data_dispatch["person_id"];
+        //      //person_id
+        //      $array_data_insert_one["person_id"] = $data_dispatch["person_id"];
 
-             //country_id
-             $array_data_insert_one["country_id"] = $data_dispatch["location_id"][0];
+        //      //country_id
+        //      $array_data_insert_one["country_id"] = $data_dispatch["location_id"][0];
 
-             //department_id
-             $array_data_insert_one["department_id"] = $data_dispatch["location_id"][1];
+        //      //department_id
+        //      $array_data_insert_one["department_id"] = $data_dispatch["location_id"][1];
 
-             //province_id
-             $array_data_insert_one["province_id"] = $data_dispatch["location_id"][2];
+        //      //province_id
+        //      $array_data_insert_one["province_id"] = $data_dispatch["location_id"][2];
 
-             //district_id
-             $array_data_insert_one["district_id"] = $data_dispatch["location_id"][3];
+        //      //district_id
+        //      $array_data_insert_one["district_id"] = $data_dispatch["location_id"][3];
 
-             //address
-             $array_data_insert_one["address"] = $data_dispatch["address"];
+        //      //address
+        //      $array_data_insert_one["address"] = $data_dispatch["address"];
 
-             //phone
-             $array_data_insert_one["phone"] = null;
+        //      //phone
+        //      $array_data_insert_one["phone"] = null;
 
-             // TODO  investigar que va en main
-             $array_data_insert_one["main"] = 0;
+        //      // TODO  investigar que va en main
+        //      $array_data_insert_one["main"] = 0;
 
-             //establishment_code
-             $array_data_insert_one["establishment_code"] = $data_dispatch["establishment_code"];
+        //      //establishment_code
+        //      $array_data_insert_one["establishment_code"] = $data_dispatch["establishment_code"];
 
-             $data_insert_all->push($array_data_insert_one);
-           }
+        //      $data_insert_all->push($array_data_insert_one);
+        //    }
 
-            DB::table('person_addresses')->insert($data_insert_all->all());
+        //     DB::table('person_addresses')->insert($data_insert_all->all());
 
-            DB::table('configurations')->update([
-                'is_migrated_address' => true
-            ]);
-         }
+        //     DB::table('configurations')->update([
+        //         'is_migrated_address' => true
+        //     ]);
+        //  }
 
     }
 }

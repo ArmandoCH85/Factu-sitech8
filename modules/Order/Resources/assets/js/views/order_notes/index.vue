cc962dfe967ca7a1d3a3fd7dc603e0299a0742a0
Cristian Vega <cristianvega73212828@gmail.com>
2026-01-15 18:10:18 -0500

    fix: boton de anular pedidos no era clickeable

--- modules/Order/Resources/assets/js/views/order_notes/index.vue
+++ modules/Order/Resources/assets/js/views/order_notes/index.vue
@@ -338,7 +338,7 @@
                                         v-if="canAnulate(row)" 
                                         :command="{action: 'anulate', id: row.id}"
                                         class="text-danger option-delete"
-                                        divided>
+                                        >
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-circle-x me-2"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" /><path d="M10 10l4 4m0 -4l-4 4" /></svg>
                                         Anular
                                     </el-dropdown-item>

5788a6faa204190492b8f69a88f95b5c6c7da5ae
Jairo <jairocalderon680@gmail.com>
2026-03-12 16:38:28 -0500

    feat(page-title): titulo de pagina actualizado para restaurante y tienda

--- modules/Ecommerce/Resources/views/layouts/master.blade.php
+++ modules/Ecommerce/Resources/views/layouts/master.blade.php
@@ -4,11 +4,12 @@
 <!-- Mirrored from portotheme.com/html/porto_ecommerce/demo-6/ by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 07 Sep 2019 03:39:38 GMT -->
 
 <head>
+    @php($pageCompany = $company ?? $vc_company ?? null)
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>eCommerce</title>
+    <title>{{ data_get($pageCompany, 'title_web') ?: data_get($pageCompany, 'trade_name') }}</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="keywords" content="ecommerce" />
     <meta name="description" content="eCommerce">
@@ -32,7 +33,7 @@
     <link rel="stylesheet" href="{{ asset('porto-light/css/styles_ecommerce.css') }}" />
 </head>
 
-<body>
+<body data-company-title="{{ data_get($pageCompany, 'title_web') ?: data_get($pageCompany, 'trade_name') }}">
     <div class="page-wrapper">
 
         @include('ecommerce::layouts.partials_ecommerce.header')

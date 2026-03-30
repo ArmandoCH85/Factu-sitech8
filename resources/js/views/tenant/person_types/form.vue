955d4fc4af71aadf0c163c0d6f91a4b9fbbf75a3
Jairo <jairocalderon680@gmail.com>
2026-03-30 10:52:52 -0500

    feat: Agregar toggle de habilitar precio en el formulario de Tipos de clientes

--- resources/js/views/tenant/person_types/form.vue
+++ resources/js/views/tenant/person_types/form.vue
@@ -20,8 +20,14 @@
                             </div> 
                     </div> 
                     <div class="col-md-6">
-                        <div class="form-group" :class="{'has-danger': errors.description}">
-                            <label class="control-label">Seleccionar Precio: </label>
+                        <div class="form-group d-flex align-items-center mb-0">
+                            <label class="control-label">Habilitar precio</label>
+                            <div class="mt-2 ms-2">
+                                <el-switch v-model="form.enabled_price" @change="onTogglePrice"></el-switch>
+                            </div>
+                        </div>
+                        <div class="form-group" :class="{'has-danger': errors.description}" v-if="form.enabled_price">
+                            <label class="control-label">Seleccionar Precio: <span class="text-danger">*</span></label>
                                     <el-select v-model="form.price_label_id"
                                             placeholder="Precio por cliente"
                                             popper-class="el-select-currency"
@@ -33,7 +39,7 @@
                                     </el-select>
                             <small class="form-control-feedback" v-if="errors.description" v-text="errors.description[0]"></small>
                         </div>
-                    </div> 
+                    </div>
                     <div class="col-md-12" v-if="form.enabled_description_person_type">
                         <div class="form-group" :class="{'has-danger': errors.description}">
                             <label class="control-label">Descripción: <span class="text-danger">*</span></label>
@@ -76,8 +82,9 @@
                 this.errors = {}
                 this.form = {
                     id: null,
-                    description: null, 
+                    description: null,
                     price_label_id: null,
+                    enabled_price: false,
                 }
             },
             create() { 
@@ -96,6 +103,10 @@
                     })
             }, 
             submit() {
+                if (this.form.enabled_price && !this.form.price_label_id) {
+                    this.$message.error('Debe seleccionar un precio.')
+                    return
+                }
                 this.loading_submit = true
                 this.$http.post(`/${this.resource}`, this.form)
                     .then(response => {
@@ -118,6 +129,11 @@
                         this.loading_submit = false
                     })
             }, 
+            onTogglePrice(val) {
+                if (!val) {
+                    this.form.price_label_id = null
+                }
+            },
             close() {
                 this.$emit('update:showDialog', false)
                 this.initForm()

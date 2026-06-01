9844da99d55e401b59206ebd015f0e31c901713e
Cristian Vega <cristianvega73212828@gmail.com>
2026-06-01 13:02:48 -0500

    fix(dispatch): Nuevo formato de fecha de inicio de traslado (obligatorio)

--- app/CoreFacturalo/Templates/xml/dispatch.blade.php
+++ app/CoreFacturalo/Templates/xml/dispatch.blade.php
@@ -170,9 +170,9 @@
                                    listName="Modalidad de traslado"
                                    listAgencyName="PE:SUNAT">{{ $document['transport_mode_type_id'] }}</cbc:TransportModeCode>
             <!-- FECHA DE INICIO DEL TRASLADO o FECHA DE ENTREGA DE BIENES AL TRANSPORTISTA -->
-            <cac:TransitPeriod>
-                <cbc:StartDate>{{ $document['date_of_shipping'] }}</cbc:StartDate>
-            </cac:TransitPeriod>
+            <cac:LoadingTransportEvent>
+			    <cbc:OccurrenceDate>{{ $document['date_of_shipping'] }}</cbc:OccurrenceDate>
+		    </cac:LoadingTransportEvent>
             @if($document['transport_mode_type_id'] === '01' && !$document['is_transport_m1l'])
                 <cac:CarrierParty>
                     <cac:PartyIdentification>

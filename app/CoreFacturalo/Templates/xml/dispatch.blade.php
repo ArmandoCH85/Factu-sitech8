f312458e6a52eca3378e06315b1012c8a80b8d70
Cristian Vega <cristianvega73212828@gmail.com>
2026-06-02 17:41:19 -0500

    fix(dispatch|xml): Condiciones respecto a la fecha de inicio de traslados | 3

--- app/CoreFacturalo/Templates/xml/dispatch.blade.php
+++ app/CoreFacturalo/Templates/xml/dispatch.blade.php
@@ -226,9 +226,9 @@
                 $carrier_vehicle_and_driver_registration_indicator = $document['transport_tuc'] && $document['transport_mode_type_id'] != '02' && $document['has_transport_driver_01'] ==true;
             @endphp
             @if (
-                $document['transport_mode_type_id'] !== '02'
-                && !($document['transport_mode_type_id'] === '01' && $document['is_transport_m1l'])
-                && !($document['transport_mode_type_id'] === '01' && !$document['is_transport_m1l'] && $carrier_vehicle_and_driver_registration_indicator)
+                $document['transport_mode_type_id'] === '02'
+                || ($document['transport_mode_type_id'] === '01' && $document['is_transport_m1l'])
+                || ($document['transport_mode_type_id'] === '01' && !$document['is_transport_m1l'] && $carrier_vehicle_and_driver_registration_indicator)
             )
             <!-- FECHA DE INICIO DEL TRASLADO o FECHA DE ENTREGA DE BIENES AL TRANSPORTISTA -->
                 <cac:LoadingTransportEvent>
